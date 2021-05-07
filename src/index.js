a2396595913fcabfb7348529825ffda322d4f58d
Rajat Venkatesh <vrajat@users.noreply.github.com>
2021-05-07 21:09:29 +0530

    chore: Change brand from kedro to tokern

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'what-input';
 import App from './components/app';
-import EasterEgg from './components/easter-egg';
 import getPipelineData from './utils/data-source';
 import './styles/index.css';
 
 const KedroViz = () => (
   <>
     <App data={getPipelineData()} />
-    <EasterEgg />
   </>
 );
 
